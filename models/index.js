21e1ce04d95f3d01717dd65ca21a6d36b60eea61
chrisvanetten <christophervanetten@gmail.com>
2021-08-31 14:23:52 -0500

    finished model associations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Product.hasOne(Category, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsTo(Category, {
+   foreignKey: 'category_id',
+});
+
+Category.hasMany(Product, {
+   foreignKey: 'category_id',
+   onDelete: 'CASCADE',
+});
+
+Product.belongsToMany(Tag, {through: 'ProductTag'});
+
+Tag.belongsToMany(Product, {through: 'ProductTag'});
+
+
 
 module.exports = {
   Product,
